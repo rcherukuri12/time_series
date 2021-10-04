70c0377c090c8f0e3488b96e2ee62c8caec52380
rcherukuri12 <raja_cherukuri@hotmail.com>
2021-10-04 06:27:45 -0700

    Create ap.py
    initial check-in

---
+++ ap.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from prophet import Prophet
+df = pd.read_csv("/Users/raja/data/AirPassengers.csv")
+df["Month"]=pd.to_datetime(df["Month"])
+df.columns = ["ds","y"]
+model = Prophet(seasonality_mode ="multiplicative")
+model.fit(df)
+future = model.make_future_dataframe(periods=365*5,freq ="MS")
+forecast = model.predict(future)
+fig=model.plot(forecast)
+plt.show()
